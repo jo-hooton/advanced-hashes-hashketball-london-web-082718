718d2d76af08378e5fee65079a4cbd6f500c812d
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-25 20:11:04 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -113,6 +113,7 @@ def game_hash
     }
 end
 
-def num_points_scored(player_name)
-
+def num_points_scored(name)
+  find_player = players.find {| player | player.fetch(:player_name) == name }
+  find_player.fetch(:points)
 end
