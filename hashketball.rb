ea02f27f0dcc8b58b45e26b3a48e1ec18378f7d9
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-25 18:49:48 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,127 @@
-# Write your code here!
+def game_hash
+  game {
+    home: { team_name:"Brooklyn Nets"
+            colors:["Black", "White"]
+            players: 
+                { "Alan Anderson" {
+                  number:
+                  shoe:
+                  points:
+                  rebounds:
+                  assists:
+                  steals:
+                  blocks:
+                  slam_dunks:
+                }
+                  
+                },
+                { "Reggie Evans" {
+                  number:
+                  shoe:
+                  points:
+                  rebounds:
+                  assists:
+                  steals:
+                  blocks:
+                  slam_dunks: 
+                 }
+                },
+                { "Brook Lopez" {
+                  number:
+                  shoe:
+                  points:
+                  rebounds:
+                  assists:
+                  steals:
+                  blocks:
+                  slam_dunks: 
+                 }
+                },
+                { "Mason Plumlee" {
+                  number:
+                  shoe:
+                  points:
+                  rebounds:
+                  assists:
+                  steals:
+                  blocks:
+                  slam_dunks: 
+                 }
+                },
+                { "Jason Terry" {
+                  number:
+                  shoe:
+                  points:
+                  rebounds:
+                  assists:
+                  steals:
+                  blocks:
+                  slam_dunks: 
+                 }
+                }
+          }
+    away: {   team_name: "Charlotte Hornets"
+              colors: ["Turquoise","Purple"]
+              players: {
+                { "Jeff Adrien" {
+                  number:
+                  shoe:
+                  points:
+                  rebounds:
+                  assists:
+                  steals:
+                  blocks:
+                  slam_dunks: 
+                 }
+                },
+                { "Bismak Biyombo" {
+                  number:
+                  shoe:
+                  points:
+                  rebounds:
+                  assists:
+                  steals:
+                  blocks:
+                  slam_dunks: 
+                 }
+                },
+                { "DeSagna Diop" {
+                    number:
+                    shoe:
+                    points:
+                    rebounds:
+                    assists:
+                    steals:
+                    blocks:
+                    slam_dunks: 
+                   }
+                },
+                { "Ben Gordon" {
+                    number:
+                    shoe:
+                    points:
+                    rebounds:
+                    assists:
+                    steals:
+                    blocks:
+                    slam_dunks: 
+                   }
+                },
+                { "Brendan Haywood" {
+                    number:
+                    shoe:
+                    points:
+                    rebounds:
+                    assists:
+                    steals:
+                    blocks:
+                    slam_dunks: 
+                   }
+                }
+      }
+    }
+  }
+end
 
 
 
